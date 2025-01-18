b66c3b441c79cd10f9fa949ff8cf8592287a949b
游宸睿 <asd47964773@gmail.com>
2025-01-18 17:30:25 +0800

    250118 daily

---
+++ mycode/1368.minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -0,0 +1,86 @@
+#
+# @lc app=leetcode id=1368 lang=python
+#
+# [1368] Minimum Cost to Make at Least One Valid Path in a Grid
+#
+
+# @lc code=start
+class Solution(object):
+    def minCost(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
+        m = len(grid)
+        n = len(grid[0])
+        
+        # dijkstra algorithm
+        min_cost = [[int(1e9) for _ in range(n)] for _ in range(m)]
+        min_cost[0][0] = 0
+        priority_queue = [(0, [0, 0])]
+        
+        while priority_queue:
+            
+            priority_queue.sort()
+            
+            current_cost, [current_i, current_j] = priority_queue.pop(0)
+            
+            if current_cost > min_cost[current_i][current_j]:
+                continue
+            
+            if current_i == m-1 and current_j ==  n-1:
+                return current_cost
+            
+            # Explore neighbors
+            ## right
+            if current_j != n-1:
+                if grid[current_i][current_j] == 1:
+                    cost = current_cost
+                else:
+                    cost = current_cost + 1
+                
+                if cost < min_cost[current_i][current_j+1]:
+                    min_cost[current_i][current_j+1] = cost
+                    priority_queue.append((cost, [current_i, current_j+1]))
+
+            ## left
+            if current_j != 0:
+                if grid[current_i][current_j] == 2:
+                    cost = current_cost
+                else:
+                    cost = current_cost + 1
+                
+                if cost < min_cost[current_i][current_j-1]:
+                    min_cost[current_i][current_j-1] = cost
+                    priority_queue.append((cost, [current_i, current_j-1]))
+                    
+            
+            ## lower
+            if current_i != m-1:
+                if grid[current_i][current_j] == 3:
+                    cost = current_cost
+                else:
+                    cost = current_cost + 1
+                
+                if cost < min_cost[current_i+1][current_j]:
+                    min_cost[current_i+1][current_j] = cost
+                    priority_queue.append((cost, [current_i+1, current_j]))
+                    
+            ## upper 
+            if current_i != 0:
+                if grid[current_i][current_j] == 4:
+                    cost = current_cost
+                else:
+                    cost = current_cost + 1
+                
+                if cost < min_cost[current_i-1][current_j]:
+                    min_cost[current_i-1][current_j] = cost
+                    priority_queue.append((cost, [current_i-1, current_j]))
+        
+        return min_cost[m-1][n-1]
+# @lc code=end
+
+print(Solution().minCost([[1,1,1,1],[2,2,2,2],[1,1,1,1],[2,2,2,2]])) # 3
+print(Solution().minCost([[1,1,3],[3,2,2],[1,1,4]])) # 0
+print(Solution().minCost([[1,2],[4,3]])) # 1
