c997fc6553ce9ef7b95ed65d2ba5d5f19255694f
Chen-Jui, Yu <asd47964773@gmail.com>
2025-04-10 17:44:40 +0800

    240410 daily

--- mycode/2999.count-the-number-of-powerful-integers.py
+++ mycode/2999.count-the-number-of-powerful-integers.py
@@ -34,9 +34,6 @@ class Solution:
         end = end.zfill(digs)
         st = st.zfill(digs)
 
-        # print(st)
-        # print(end)
-
         if st[:-1] == end[:-1]:
             t = min(int(end[-1]), limit)
             if t < int(st[-1]):
@@ -49,10 +46,6 @@ class Solution:
             al = 1
             for i in range(digs-1, 0, -1):
 
-                # print("#", al)
-                # print(st, end)
-                # print("-")
-
                 if not any(int(digit) > limit for digit in end[:i]):
                     if int(end[i]) <= limit:
                         if al == 1:
@@ -61,8 +54,7 @@ class Solution:
                             cnt += int(end[i]) * al
                     else:
                         cnt += (limit + 1) * al
-                    
-                    # print("end", (min(limit, int(end[i])) + 1))
+                
 
                 if not any(int(digit) > limit for digit in st[:i+1]):
                     if al == 1:
@@ -70,40 +62,16 @@ class Solution:
                     else:
                         cnt += (limit - int(st[i])) * al
 
-                    # print("st", (limit - int(st[i]) + 1))
-
                 al *= (limit+1)
+            
 
-                # print("-------")
-
-            # print("cnt", cnt)
             if int(st[0]) <= limit:
                 if int(end[0]) > limit:
                     cnt += (limit-int(st[0]))*al
                 else:
                     cnt += (int(end[0])-int(st[0])-1)*al
 
-
-
-            # al = int(end[:-1]) - int(st[:-1]) -1
-            # cnt = (limit+1)*al
-
-            # print(cnt)
-
-            # print("aa", limit, int(end[-1]))
-            
-            # if not any(int(digit) > limit for digit in end[:-1]):
-            #     cnt += min(limit, int(end[-1])) + 1
-            
-            # print(cnt)
-
-            # if int(st[-1]) <= limit:
-            #     cnt += limit - int(st[-1]) + 1
-
-
         return cnt
             
-            
-        
 # @lc code=end
 
