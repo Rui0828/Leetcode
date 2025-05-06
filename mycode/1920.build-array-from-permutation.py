4368453a275f8d35ecbd8ba760c95caa95560aae
Chen-Jui, Yu <asd47964773@gmail.com>
2025-05-06 12:01:51 +0800

    250506 daily

---
+++ mycode/1920.build-array-from-permutation.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1920 lang=python3
+#
+# [1920] Build Array from Permutation
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        
+        return ans
+        
+# @lc code=end
+
