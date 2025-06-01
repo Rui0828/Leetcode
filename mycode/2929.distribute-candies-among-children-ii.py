ef53284238cf454e994c02d4db260e1e8ea6e2b5
游宸睿 <asd47964773@gmail.com>
2025-06-01 16:33:56 +0800

    250601 daily

---
+++ mycode/2929.distribute-candies-among-children-ii.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=2929 lang=python3
+#
+# [2929] Distribute Candies Among Children II
+#
+import math
+
+# @lc code=start
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        
+        all_cnt = math.comb(n+2, 2)
+        s1 = math.comb(n-limit+1, 2) if n-limit+1 > 0 else 0
+        s2 = math.comb(n-2*limit, 2) if n-2*limit > 0 else 0
+        s3 = math.comb(n-3*limit-1, 2) if n-3*limit-1 > 0 else 0
+
+        return all_cnt - 3 * s1 + 3 * s2 - s3
+        
+# @lc code=end
+
