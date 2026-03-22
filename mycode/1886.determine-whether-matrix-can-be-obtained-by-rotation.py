f4d472d7678702d00c833d310c99fee653d7429b
Chen-Jui, Yu <rui0828@users.noreply.github.com>
2026-03-22 17:58:56 +0800

    260322 daily

---
+++ mycode/1886.determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=1886 lang=python3
+#
+# [1886] Determine Whether Matrix Can Be Obtained By Rotation
+#
+
+# @lc code=start
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        
+        for _ in range(4):
+            if mat==target:
+                return True
+            mat = [list(row) for row in list(zip(*mat[::-1]))]
+
+        return False
+        
+# @lc code=end
+
